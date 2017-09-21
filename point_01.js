cf73d5a627d45c94258e866cd3b89fd82984a3f8
Aureliano <sersinfin@gmail.com>
2017-09-21 09:17:01 -0700

    Initial p1 branch commit.

--- point_01.js
+++ point_01.js
@@ -1,21 +1,11 @@
-// Project 2, Version 1.2
-// Using system variable "with"
+// Project 1 
 
-var x = 10;
 
 function setup() {
-    createCanvas (480, 220);
 
 }
 
 function draw() {
-  background(20,200,150);
+  background(200);
 
-  strokeWeight(2); 
-  //first parameter using addition assignment operator
-  point( x+=1, 10 );
-  // if condition using system varialbe "with"
-    if( x > width/2 ) {
-     x = width/2;   
-    }
 }
