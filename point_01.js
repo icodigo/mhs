a92059c47a1c635e50111d8a9fd820af37b96c2f
Aureliano Nava <sersinfin@gmail.com>
2017-09-23 20:31:18 -0700

    Simplified the start for Project 2.
    Use this as the start of Project 2.

--- point_01.js
+++ point_01.js
@@ -1,21 +1,12 @@
-// Project 2, Version 1.2
-// Using system variable "with"
-
-var x = 10;
+// Project 2 -- Beginning Template for Project 2
 
 function setup() {
     createCanvas (480, 220);
 
-}
+    }
 
 function draw() {
-  background(20,200,150);
-
+  background(200);
   strokeWeight(2); 
-  //first parameter using addition assignment operator
-  point( x+=1, 10 );
-  // if condition using system varialbe "with"
-    if( x > width/2 ) {
-     x = width/2;   
-    }
+  point(25, 25);
 }
